ec9898144880f10a2f3b8316498a9cf70a3da872
hhz04 <hhz04@sina.com>
2020-04-08 01:41:20 +0800

    Found that there was already an incomplete Sekiro class and merged them

--- core/src/com/towerdefense/game/SekiroDieTwice.java
+++ core/src/com/towerdefense/game/SekiroDieTwice.java
@@ -4,14 +4,18 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class SekiroDieTwice extends Monster{
 
+	boolean isRevive;
 	SekiroDieTwice(Texture texture, Tile startile,Map map, int height, int width, int atk, int speed) {
 		super(texture, startile, map, height, width, atk, speed);
-		this.hp = 10;
-	}
-	
-	public void regen()
-	{
-		hp+=50;
+		this.hp = 50;
 	}
 
+	public void revive()
+	{
+		if(isDead()&&!(isRevive))
+		{
+			hp = originalHp/2;
+			isRevive = true;
+		}
+	}
 }
