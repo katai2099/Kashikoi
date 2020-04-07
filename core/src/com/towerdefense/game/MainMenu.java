77c8243b3340173c95ba7b869ea677a4740850ac
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2020-04-07 18:43:53 +0200

    Add some functions

--- core/src/com/towerdefense/game/MainMenu.java
+++ core/src/com/towerdefense/game/MainMenu.java
@@ -5,14 +5,28 @@ import com.badlogic.gdx.graphics.Texture;
 public class MainMenu {
 
 	private Texture background;
-	
-	public MainMenu()
-	{
+
+	public MainMenu() {
 		background = new Texture("grass.png");
 	}
-	
-	public void update()
-	{
+
+	public void update() {
 	}
-	
+
+	public void NewGame() {
+
+	}
+
+	public void SelectStage() {
+
+	}
+
+	public void HowToPlay() {
+
+	}
+
+	public void Exit() {
+
+	}
+
 }
