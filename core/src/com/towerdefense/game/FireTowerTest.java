0919f1440059eec53a533fc44659ae2199778416
Phommachanh Sayalath <kataix2@gmail.com>
2020-04-23 00:14:18 +0200

    rm firetowertest

--- core/src/com/towerdefense/game/FireTowerTest.java
+++ core/src/com/towerdefense/game/FireTowerTest.java
@@ -1,21 +1,11 @@
 package com.towerdefense.game;
 
-import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+
 
 class FireTowerTest {
 
-	@Test
-	void testDamageMonster() {
-		Onion t = new Onion();
-		FireTower k = new FireTower();
-		
-		k.damageMonster(t); 
-		
-		float hp = t.getHp();
-		float expected = 2.0f;
-		assertEquals(expected,hp,0.0f);
-	}
+	
+	
 
 }
